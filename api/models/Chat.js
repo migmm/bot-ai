5daaa3b7ae8c6fa4910df8ef8a191c45a8beda93
Miguel <micmigm@gmail.com>
2024-12-29 01:17:45 -0300

    Updated chat in order to get chat history joined to an userid

--- api/models/Chat.js
+++ api/models/Chat.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 
 const ChatSchema = new mongoose.Schema({
-    customerId: String,
-    timestamp: { type: Date, default: Date.now },
-    message: String,
-    response: String,
-    consultationNumber: String
+    customerId: { type: String, unique: true },
+    messages: [{
+        timestamp: { type: Date, default: Date.now },
+        message: String,
+        response: String
+    }],
+    lastActivity: { type: Date, default: Date.now }
 }, { strict: false });
 
 export default mongoose.model('Chat', ChatSchema);
