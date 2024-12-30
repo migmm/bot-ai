a5f783694af8ad140f5ea6618d6e2c72e32c3226
Miguel <micmigm@gmail.com>
2024-12-30 16:50:12 -0300

    Temporarly productcs hardcoded

--- api/utils/loadProducts.js
+++ api/utils/loadProducts.js
@@ -1,13 +1,14 @@
-import { read as xlsxRead, utils } from 'xlsx';
+import xlsx from 'xlsx';
 import config from '../config/config.js';
 
+
+
+
 export const loadProducts = () => {
-    try {
-        const workbook = xlsxRead(config.excelPath);
-        const sheet = workbook.Sheets[workbook.SheetNames[0]];
-        return utils.sheet_to_json(sheet);
-    } catch (error) {
-        console.error('Error loading products from Excel sheet:', error);
-        return [];
-    }
+    return {
+        products: [
+            { Nombre: "Laptop HP Pavilion", Categoría: "laptops", Marca: "hp", Precio: 1200, Descripción: "Laptop con pantalla Full HD y 8GB de RAM." },
+            { Nombre: "Laptop Dell Inspiron", Categoría: "laptops", Marca: "dell", Precio: 1100, Descripción: "Laptop con procesador i5 y SSD de 256GB." },
+        ],
+    };
 };
