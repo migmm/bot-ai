c378e73c418089c1489ce077ef6fa958d4a9a544
Miguel <micmigm@gmail.com>
2025-01-01 22:19:43 -0300

    Create promos data

---
+++ api/data/promos.js
@@ -0,0 +1,20 @@
+export const promos = [
+    {
+        title: "Descuento 20% en Rollos de Atún",
+        description: "Obtén un 20% de descuento en todos los rollos de atún hasta el 31 de enero.",
+        discount: 20,
+        validUntil: new Date("2025-01-31T23:59:59.000Z")
+    },
+    {
+        title: "2x1 en Sushi Rolls",
+        description: "Llévate dos sushi rolls por el precio de uno durante todo el mes de febrero.",
+        discount: 50,
+        validUntil: new Date("2025-02-28T23:59:59.000Z")
+    },
+    {
+        title: "Descuento 10% en pedidos grandes",
+        description: "Obtén un 10% de descuento en pedidos superiores a $50. Válido hasta el 28 de febrero.",
+        discount: 10,
+        validUntil: new Date("2025-02-28T23:59:59.000Z")
+    }
+];
