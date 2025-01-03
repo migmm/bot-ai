2dd5741c38bacc5b4b4d355ad9997ee7ee495940
Miguel <micmigm@gmail.com>
2025-01-02 22:06:09 -0300

    Changed route name

--- api/routes/chatRoutes.js
+++ api/routes/chatRoutes.js
@@ -4,6 +4,6 @@ import { sendMessage, getChatHistory } from '../controllers/chatController.js';
 const router = express.Router();
 
 router.post('/chat', sendMessage);
-router.get('/chat-history/:customerId', getChatHistory);
+router.get('/order/:customerId', getChatHistory);
 
 export default router;
