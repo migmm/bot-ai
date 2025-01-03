02224252a6d5694d4775f1e5f157978a30f2546a
Miguel <micmigm@gmail.com>
2025-01-02 22:06:39 -0300

    Created holidays data

---
+++ api/data/holidays.js
@@ -0,0 +1,43 @@
+// api/data/holidays.js
+const holidays = [
+    // Enero
+    { date: "2025-01-01", name: "Año Nuevo", reopenDate: "2025-01-02" },
+    
+    // Febrero
+    { date: "2025-02-24", name: "Carnaval", reopenDate: "2025-02-25" },
+    { date: "2025-02-25", name: "Carnaval", reopenDate: "2025-02-26" },
+    
+    // Marzo
+    { date: "2025-03-24", name: "Día Nacional de la Memoria por la Verdad y la Justicia", reopenDate: "2025-03-25" },
+    
+    // Abril
+    { date: "2025-04-02", name: "Día del Veterano y de los Caídos en la Guerra de Malvinas", reopenDate: "2025-04-03" },
+    { date: "2025-04-17", name: "Jueves Santo", reopenDate: "2025-04-18" },
+    { date: "2025-04-18", name: "Viernes Santo", reopenDate: "2025-04-19" },
+    
+    // Mayo
+    { date: "2025-05-01", name: "Día del Trabajador", reopenDate: "2025-05-02" },
+    { date: "2025-05-25", name: "Día de la Revolución de Mayo", reopenDate: "2025-05-26" },
+    
+    // Junio
+    { date: "2025-06-17", name: "Día Paso a la Inmortalidad del Gral. Martín Miguel de Güemes", reopenDate: "2025-06-18" },
+    { date: "2025-06-20", name: "Día Paso a la Inmortalidad del Gral. Manuel Belgrano", reopenDate: "2025-06-21" },
+    
+    // Julio
+    { date: "2025-07-09", name: "Día de la Independencia", reopenDate: "2025-07-10" },
+    
+    // Agosto
+    { date: "2025-08-17", name: "Paso a la Inmortalidad del Gral. José de San Martín", reopenDate: "2025-08-18" },
+    
+    // Octubre
+    { date: "2025-10-12", name: "Día del Respeto a la Diversidad Cultural", reopenDate: "2025-10-13" },
+    
+    // Noviembre
+    { date: "2025-11-20", name: "Día de la Soberanía Nacional", reopenDate: "2025-11-21" },
+    
+    // Diciembre
+    { date: "2025-12-08", name: "Inmaculada Concepción de María", reopenDate: "2025-12-09" },
+    { date: "2025-12-25", name: "Navidad", reopenDate: "2025-12-26" }
+];
+
+export default holidays;
