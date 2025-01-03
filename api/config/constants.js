48a09b3a78537f1027bb78c973305a2547f97067
Miguel <micmigm@gmail.com>
2025-01-02 22:04:33 -0300

    Added new constants

--- api/config/constants.js
+++ api/config/constants.js
@@ -8,6 +8,11 @@ export const config = {
     llmModel: process.env.LLM_MODEL,
     llmSystemPrompt: process.env.SYSTEM_PROMPT,
     serverPort: process.env.PORT,
-    mongoDbUri: process.env.MONGODB_URI
+    mongoDbUri: process.env.MONGODB_URI,
+    classificationPrompt: process.env.CLASSIFICATION_PROMPT,
+    llmSystemPrompt: process.env.SYSTEM_PROMPT,
+    temperature: parseFloat(process.env.TEMPERATURE),
+    maxTokens: parseInt(process.env.MAX_TOKENS),
+    locales: process.env.LOCALES
 };
 
