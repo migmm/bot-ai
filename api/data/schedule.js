1682f331778f2686feefe09f8b5229c2666bccf1
Miguel <micmigm@gmail.com>
2025-01-02 22:06:50 -0300

    Created Schedule data

---
+++ api/data/schedule.js
@@ -0,0 +1,46 @@
+const schedule = [
+    {
+        day: "Monday",
+        openTime: "09:00 AM",
+        closeTime: "10:00 PM",
+        isDeliveryAvailable: true
+    },
+    {
+        day: "Tuesday",
+        openTime: "09:00 AM",
+        closeTime: "10:00 PM",
+        isDeliveryAvailable: true
+    },
+    {
+        day: "Wednesday",
+        openTime: "09:00 AM",
+        closeTime: "10:00 PM",
+        isDeliveryAvailable: true
+    },
+    {
+        day: "Thursday",
+        openTime: "09:00 AM",
+        closeTime: "10:00 PM",
+        isDeliveryAvailable: true
+    },
+    {
+        day: "Friday",
+        openTime: "09:00 AM",
+        closeTime: "11:00 PM",
+        isDeliveryAvailable: true
+    },
+    {
+        day: "Saturday",
+        openTime: "10:00 AM",
+        closeTime: "11:00 PM",
+        isDeliveryAvailable: true
+    },
+    {
+        day: "Sunday",
+        openTime: "10:00 AM",
+        closeTime: "09:00 PM",
+        isDeliveryAvailable: true
+    }
+];
+
+export default schedule;
