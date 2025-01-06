9478bd024c02a85b9d9618c04f3335e4825a6485
Miguel <micmigm@gmail.com>
2025-01-06 15:19:34 -0300

    Added validations

--- api/routes/businessInfoRoutes.js
+++ api/routes/businessInfoRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getBusinessInfo, updateBusinessInfo } from '../controllers/businessInfoController.js';
+import { validate, schemas } from '../middlewares/validation.js';
 
 const router = express.Router();
 
 router.get('/business-info', getBusinessInfo);
-router.put('/business-info/:id', updateBusinessInfo);
+router.put('/business-info/:id', validate(schemas.businessInfo), updateBusinessInfo);
 
 export default router;
