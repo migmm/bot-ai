beb66669c3dd3e1454b9cf8b237b20bd1bfca933
Miguel <micmigm@gmail.com>
2025-01-06 15:19:45 -0300

    added validations

--- api/routes/chatRoutes.js
+++ api/routes/chatRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { sendMessage,} from '../controllers/chatController.js';
+import { validate, schemas } from '../middlewares/validation.js';
 
 const router = express.Router();
 
-router.post('/chat', sendMessage);
+router.post('/chat', validate(schemas.chat), sendMessage);
 
 
 export default router;
