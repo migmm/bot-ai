f57b5882678dd8fdbe51df59a1b67214df37d823
Miguel <micmigm@gmail.com>
2025-01-05 22:36:23 -0300

    crerated business info data

---
+++ api/data/businessInfo.js
@@ -0,0 +1,11 @@
+const businessInfo = {
+    name: "El japones",
+    address: "Güemes 3000",
+    city: "CABA",
+    state: "Buenos Aires",
+    zip: "1425",
+    phone: "555-555-5555",
+    email: "info@eljapones.com",
+}
+
+export default businessInfo;
