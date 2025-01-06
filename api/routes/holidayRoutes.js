5a0373436b94b2d65dfe94f0ee49a3e1c07fd797
Miguel <micmigm@gmail.com>
2025-01-06 15:19:54 -0300

    added validations

--- api/routes/holidayRoutes.js
+++ api/routes/holidayRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getHolidays, addHoliday } from '../controllers/holidayController.js';
+import { validate, schemas } from '../middlewares/validation.js';
 
 const router = express.Router();
 
 router.get('/holidays', getHolidays);
-router.post('/holidays', addHoliday);
+router.post('/holidays', validate(schemas.holiday), addHoliday);
 
 export default router;
