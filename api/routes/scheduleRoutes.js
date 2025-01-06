d8b5d29bbf4381d93e3191add1705d2745733f7c
Miguel <micmigm@gmail.com>
2025-01-06 15:20:01 -0300

    Added validations

--- api/routes/scheduleRoutes.js
+++ api/routes/scheduleRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getSchedule, updateSchedule } from '../controllers/scheduleController.js';
+import { validate, schemas } from '../middlewares/validation.js';
 
 const router = express.Router();
 
 router.get('/schedule', getSchedule);
-router.put('/schedule/:id', updateSchedule);
+router.put('/schedule/:id',  validate(schemas.schedule), updateSchedule);
 
 export default router;
