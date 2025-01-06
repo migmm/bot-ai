d67f1009d26910445405e823edc4173f39f31e0a
Miguel <micmigm@gmail.com>
2025-01-06 15:20:07 -0300

    Added validations

--- api/routes/promoRoutes.js
+++ api/routes/promoRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getPromos, addPromo } from '../controllers/promoController.js';
+import { validate, schemas } from '../middlewares/validation.js';
 
 const router = express.Router();
 
 router.get('/promos', getPromos);
-router.post('/promos', addPromo);
+router.post('/promos',  validate(schemas.promo), addPromo);
 
 export default router;
