49f18bcc333b7e63fb39f6dfc694d34f6d665865
Miguel <micmigm@gmail.com>
2025-01-08 17:54:38 -0300

    Added function to create schedule

--- api/controllers/scheduleController.js
+++ api/controllers/scheduleController.js
@@ -9,6 +9,16 @@ export const getSchedule = async (req, res) => {
     }
 };
 
+export const createSchedule = async (req, res) => {
+    try {
+        const newSchedule = new Schedule(req.body);
+        const savedSchedule = await newSchedule.save();
+        res.status(201).json(savedSchedule);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al crear el horario' });
+    }
+};
+
 export const updateSchedule = async (req, res) => {
     try {
         const updatedSchedule = await Schedule.findByIdAndUpdate(req.params.id, req.body, { new: true });
