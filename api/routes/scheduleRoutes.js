e357af0d4629ae001cb65f519780e4b8d701fe2c
Miguel <micmigm@gmail.com>
2025-01-08 17:55:19 -0300

    Added new schedule route

--- api/routes/scheduleRoutes.js
+++ api/routes/scheduleRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getSchedule, updateSchedule } from '../controllers/scheduleController.js';
+import { getSchedule, createSchedule, updateSchedule } from '../controllers/scheduleController.js';
 import { validate, schemas } from '../middlewares/validation.js';
 
 const router = express.Router();
 
 router.get('/schedule', getSchedule);
-router.put('/schedule/:id',  validate(schemas.schedule), updateSchedule);
+router.post('/schedule', validate(schemas.schedule), createSchedule);
+router.put('/schedule/:id', validate(schemas.schedule), updateSchedule);
 
 export default router;
